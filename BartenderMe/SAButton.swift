8608c8790e290efb6118e8045c98718c7c0c7d55
Bailey Olson <baileyolson48@yahoo.com>
2020-04-21 16:40:01 -0500

    new

---
+++ BartenderMe/SAButton.swift
@@ -0,0 +1,9 @@
+//
+//  SAButton.swift
+//  BartenderMe!
+//
+//  Created by Bailey Olson on 4/17/20.
+//  Copyright © 2020 Bailey Olson. All rights reserved.
+//
+
+import Foundation
--- BartenderMe/ViewController.swift
+++ BartenderMe/ViewController.swift
@@ -7,8 +7,58 @@
 //
 
 import UIKit
-
+import MessageUI
+import SwiftUI
 
 class ViewController: UIViewController {
 
+    @IBAction func emailClicked(_ sender: Any) {
+        // This needs to be ran on a device
+        showMailComposer()
+    }
+    
+    
+    func showMailComposer() {
+        
+        guard MFMailComposeViewController.canSendMail() else {
+            //Show alert informing the user
+            return
+        }
+        
+        let composer = MFMailComposeViewController()
+        composer.mailComposeDelegate = self
+        composer.setToRecipients(["support@seanallen.co"])
+        composer.setSubject("HELP!")
+        composer.setMessageBody("I love your videos, but... help!", isHTML: false)
+        
+        present(composer, animated: true)
+    }
+}
+
+
+extension ViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        if let _ = error {
+            //Show error alert
+            controller.dismiss(animated: true)
+            return
+        }
+        
+        switch result {
+        case .cancelled:
+            print("Cancelled")
+        case .failed:
+            print("Failed to send")
+        case .saved:
+            print("Saved")
+        case .sent:
+            print("Email Sent")
+        @unknown default:
+            break
+        }
+        
+        controller.dismiss(animated: true)
+    }
 }
